735a202354228d2480eaf52a8b56c06726d0d8d1
Michael Chourdakis <windowssnt@gmail.com>
2024-07-17 21:06:54 +0300

    Specific Algorithm Scores

--- function.php
+++ function.php
@@ -17,6 +17,10 @@ $mustprepare = 0;
 $mysqli = null;
 $superadmin = 0;
 
+// ClassTypes for contests
+// 101 Mousika Sxoleia Metathesis
+
+
 $def_xml_proson = <<<XML
 <root>
     <classes>
@@ -353,7 +357,7 @@ function PrepareDatabase($msql = 0)
     QQ(sprintf("CREATE TABLE IF NOT EXISTS PROSONFORCE (ID INTEGER PRIMARY KEY %s,UID INTEGER,CID INTEGER,PLACEID INTEGER,POS INTEGER,PIDCLASS INTEGER,SCORE TEXT,FOREIGN KEY (UID) REFERENCES USERS(ID),FOREIGN KEY (CID) REFERENCES CONTESTS(ID),FOREIGN KEY (PLACEID) REFERENCES PLACES(ID),FOREIGN KEY (POS) REFERENCES POSITIONS(ID))",$j));
 //      QQ(sprintf("CREATE TABLE IF NOT EXISTS PROSONEV (ID INTEGER PRIMARY KEY %s,UID INTEGER,EVUID INTEGER,RESULT INTEGER,FOREIGN KEY (UID) REFERENCES USERS(ID),FOREIGN KEY (PID) REFERENCES PROSON(ID))",$j));
 
-    QQ(sprintf("CREATE TABLE IF NOT EXISTS CONTESTS (ID INTEGER PRIMARY KEY %s,UID INTEGER,MINISTRY TEXT,CATEGORY TEXT,DESCRIPTION TEXT,STARTDATE INTEGER,ENDDATE INTEGER,FOREIGN KEY (UID) REFERENCES USERS(ID))",$j));
+    QQ(sprintf("CREATE TABLE IF NOT EXISTS CONTESTS (ID INTEGER PRIMARY KEY %s,UID INTEGER,MINISTRY TEXT,CATEGORY TEXT,DESCRIPTION TEXT,STARTDATE INTEGER,ENDDATE INTEGER,CLASSTYPE INTEGER,FOREIGN KEY (UID) REFERENCES USERS(ID))",$j));
     QQ(sprintf("CREATE TABLE IF NOT EXISTS PLACES (ID INTEGER PRIMARY KEY %s,CID INTEGER,PARENTPLACEID INTEGER,DESCRIPTION TEXT,FOREIGN KEY (CID) REFERENCES CONTESTS(ID),FOREIGN KEY (PARENTPLACEID) REFERENCES PLACES(ID))",$j));
     QQ(sprintf("CREATE TABLE IF NOT EXISTS POSITIONS (ID INTEGER PRIMARY KEY %s,CID INTEGER,PLACEID INTEGER,DESCRIPTION TEXT,COUNT INTEGER,FOREIGN KEY (CID) REFERENCES CONTESTS(ID),FOREIGN KEY (PLACEID) REFERENCES PLACES(ID))",$j));
     QQ(sprintf("CREATE TABLE IF NOT EXISTS POSITIONGROUPS (ID INTEGER PRIMARY KEY %s,CID INTEGER,GROUPLIST TEXT,FOREIGN KEY (CID) REFERENCES CONTESTS(ID))",$j));
@@ -1250,166 +1254,7 @@ function ProsonResolutAndOrNot($uid,$pid,&$checked = array(),$deep = 0,&$reason 
 
 }
 
-function CalculateScore($uid,$cid,$placeid,$posid,$debug = 0,&$linkssave = array(),$prosononly = 0,&$desc = array(),$forwhichplace = 0,$forwhichpos = 0)
-{
-    global $rejr,$xmlp,$required_check_level;
-    EnsureProsonLoaded();
-    $pr = Single("USERS","ID",$uid);
-    if (!$pr)
-        return -1;
-    $contestrow = Single("CONTESTS","ID",$cid); 
-    if (!$contestrow)
-        return -1;
-    $posr = Single("POSITIONS","ID",$posid); 
-    $score = 0;
-
-
-
-    // If we have a posid, search for generic by thesi name
-    // else we search for generic
-    $thesiname = '';
-    if ($posr)
-        $thesiname = $posr['DESCRIPTION'];
-    $CountGeneral = QQ("SELECT COUNT(*) FROM REQS2 WHERE CID = ? AND PLACEID = 0 AND POSID = 0 AND FORTHESI = ?",array($cid,$thesiname))->fetchArray()[0];
-    if ($CountGeneral && $thesiname != '')
-        $q1 = QQ("SELECT * FROM REQS2 WHERE CID = ? AND PLACEID = 0 AND POSID = 0 AND FORTHESI = ?",array($cid,$thesiname));
-    else
-        $q1 = QQ("SELECT * FROM REQS2 WHERE CID = ? AND PLACEID = ? AND POSID = ? AND (FORTHESI IS NULL OR FORTHESI = '')",array($cid,$placeid,$posid));
-    while($r1 = $q1->fetchArray())
-    {
-        $desc2 = array();
-        $sp = $r1['SCORE'];
-        $rootc = RootForClassId($xmlp->classes,$r1['PROSONTYPE']);
-        if ($rootc)
-            $params_root = $rootc->params;
-        if ($params_root)
-            {
-                foreach($params_root->p as $param)
-                {
-                    $pa = $param->attributes();                              
-                    $partypes[(int)$pa['id']] = $pa['t'];                       
-                }    
-            }
-        $wouldeval = 0;
-        if (strstr($sp,'$values'))
-        {
-            $wouldeval = 1;
-        }
-
-        $min_needed = 1;
-        if ((int)$r1['MINX'] > 0)
-            $min_needed = (int)$r1['MINX'];
-        $max_needed = 0;
-        if ((int)$r1['MAXX'] > 0)
-            $max_needed = (int)$r1['MAXX'];
-
-        for($deep = 0 ; ; $deep++)
-        {
-            if ($deep > 0 && $max_needed > 0 && $deep >= $max_needed)
-                break;
-            $checked = array();
-            $reason = '';
-
-            $haslist = array();
-            $has = ProsonResolutAndOrNot($uid,$r1['ID'],$checked,$deep,$reason,$haslist);
-
-            if ($has != 1)
-            {
-                if ($sp > 0 || $wouldeval == 1 || $deep >= $min_needed)
-                    break; // not required
-                $rootc = RootForClassId($xmlp->classes,$r1['PROSONTYPE']);
-                $rejr = sprintf('Λείπει προαπαιτούμενο προσόν: %s %s x%s',$rootc->attributes()['t'],$reason,$min_needed);
-                return -1;    
-            }   
-
-            // He has it, 
-            if ($wouldeval || $prosononly)
-            {
-                $sp = $r1['SCORE'];
-                $deeps = $deep;
-                $time = time();
-                $qpr = QQ("SELECT * FROM PROSON WHERE UID = ? AND CLASSID = ? AND STATE >= ? AND STARTDATE < ? AND (ENDDATE > ? OR ENDDATE = 0)",array($uid,$r1['PROSONTYPE'],$required_check_level,$time,$time));
-        
-                while($rpr = $qpr->fetchArray())
-                {
-                   if ($deeps > 0)
-                    {
-                        $deeps--;
-                        continue;
-                    }
-                    $pars = QQ("SELECT * FROM PROSONPAR WHERE PID = ?",array($rpr['ID']));
-                    while($par = $pars->fetchArray())
-                    {
-                        $p_idx = $par['PIDX'];
-                        $p_val = $par['PVALUE'];
-
-                        // Check if it's date
-                        if ($partypes[$p_idx] == 3)
-                        {
-                            $startwhen = $rpr['STARTDATE'];
-                            $now = time();
-                            if ($now > $startwhen)
-                            {
-                                $a1 = From360ToActual($p_val);
-                                $a1 += ($now - $startwhen);
-                                $p_val = FromActualTo360($a1);
-                            }
-                        }
-                        if ($p_val == '') $p_val = 0;
-                        $sp = str_replace(sprintf('$values[%s]',$p_idx),$p_val,$sp);
-                    }
-                }
-                if (strstr($sp,'$values'))
-                    $sp = 0;
-                else
-                    $sp = eval2($sp,$uid,$cid,$placeid,$posid);
-            }
-
-        if ($debug)
-            {
-                if ($sp > 0)
-                    printf("%s: %s<br>",$rootc->attributes()['t'],$sp);
-            }
-
-
-        //  Forced
-        $forcedscore = QQ("SELECT * FROM PROSONFORCE WHERE (UID = ? OR UID = 0) AND (CID = ? OR CID = 0) AND (PLACEID = ? OR PLACEID = ? OR PLACEID = 0) AND (POS = ? OR POS = ? OR POS = 0) AND (PIDCLASS = ? OR PIDCLASS = 0)",array($uid,$cid,$placeid,$forwhichplace,$posid,$forwhichpos,$r1['PROSONTYPE']))->fetchArray();
-        if ($forcedscore)
-            {
-                $sp = $forcedscore['SCORE'];
-            }
-
-
-        $desc2['s'] = $sp;
-        $desc2['h'] = $haslist;
-        
-        $desc[] = $desc2;
-        $score += $sp;
-        }
-
-        
-    }
-
-
-    if ($posid)
-    {
-        $v = CalculateScore($uid,$cid,$placeid,0,$debug,$linkssave,$prosononly,$desc,0,$posid);
-        if ($v == -1)
-            return -1;
-        $score += $v;
-    }
-    else
-    if ($placeid)
-    {
-        $v =  CalculateScore($uid,$cid,0,0,$debug,$linkssave,$prosononly,$desc,$placeid,$forwhichpos);
-        if ($v == -1)
-            return -1;
-        $score += $v;
-    }
-
-    return $score;
-
-}
+require_once "score.php";
 
 $push3_admin = 1;
 require_once "push3.php";
---
+++ score.php
@@ -0,0 +1,171 @@
+<?php
+
+
+// Scores for metathesi music schools algorithm for example
+
+function CalculateScoreFor101($uid,$cid,$placeid,$posid,&$desc = array())
+{
+
+}
+
+
+function CalculateScore($uid,$cid,$placeid,$posid,$debug = 0,&$linkssave = array(),$prosononly = 0,&$desc = array(),$forwhichplace = 0,$forwhichpos = 0)
+{
+    global $rejr,$xmlp,$required_check_level;
+    EnsureProsonLoaded();
+    $pr = Single("USERS","ID",$uid);
+    if (!$pr)
+        return -1;
+    $contestrow = Single("CONTESTS","ID",$cid); 
+    if (!$contestrow)
+        return -1;
+    $posr = Single("POSITIONS","ID",$posid); 
+    $score = 0;
+
+
+
+    // If we have a posid, search for generic by thesi name
+    // else we search for generic
+    $thesiname = '';
+    if ($posr)
+        $thesiname = $posr['DESCRIPTION'];
+    $CountGeneral = QQ("SELECT COUNT(*) FROM REQS2 WHERE CID = ? AND PLACEID = 0 AND POSID = 0 AND FORTHESI = ?",array($cid,$thesiname))->fetchArray()[0];
+    if ($CountGeneral && $thesiname != '')
+        $q1 = QQ("SELECT * FROM REQS2 WHERE CID = ? AND PLACEID = 0 AND POSID = 0 AND FORTHESI = ?",array($cid,$thesiname));
+    else
+        $q1 = QQ("SELECT * FROM REQS2 WHERE CID = ? AND PLACEID = ? AND POSID = ? AND (FORTHESI IS NULL OR FORTHESI = '')",array($cid,$placeid,$posid));
+    while($r1 = $q1->fetchArray())
+    {
+        $desc2 = array();
+        $sp = $r1['SCORE'];
+        $rootc = RootForClassId($xmlp->classes,$r1['PROSONTYPE']);
+        if ($rootc)
+            $params_root = $rootc->params;
+        if ($params_root)
+            {
+                foreach($params_root->p as $param)
+                {
+                    $pa = $param->attributes();                              
+                    $partypes[(int)$pa['id']] = $pa['t'];                       
+                }    
+            }
+        $wouldeval = 0;
+        if (strstr($sp,'$values'))
+        {
+            $wouldeval = 1;
+        }
+
+        $min_needed = 1;
+        if ((int)$r1['MINX'] > 0)
+            $min_needed = (int)$r1['MINX'];
+        $max_needed = 0;
+        if ((int)$r1['MAXX'] > 0)
+            $max_needed = (int)$r1['MAXX'];
+
+        for($deep = 0 ; ; $deep++)
+        {
+            if ($deep > 0 && $max_needed > 0 && $deep >= $max_needed)
+                break;
+            $checked = array();
+            $reason = '';
+
+            $haslist = array();
+            $has = ProsonResolutAndOrNot($uid,$r1['ID'],$checked,$deep,$reason,$haslist);
+
+            if ($has != 1)
+            {
+                if ($sp > 0 || $wouldeval == 1 || $deep >= $min_needed)
+                    break; // not required
+                $rootc = RootForClassId($xmlp->classes,$r1['PROSONTYPE']);
+                $rejr = sprintf('Λείπει προαπαιτούμενο προσόν: %s %s x%s',$rootc->attributes()['t'],$reason,$min_needed);
+                return -1;    
+            }   
+
+            // He has it, 
+            if ($wouldeval || $prosononly)
+            {
+                $sp = $r1['SCORE'];
+                $deeps = $deep;
+                $time = time();
+                $qpr = QQ("SELECT * FROM PROSON WHERE UID = ? AND CLASSID = ? AND STATE >= ? AND STARTDATE < ? AND (ENDDATE > ? OR ENDDATE = 0)",array($uid,$r1['PROSONTYPE'],$required_check_level,$time,$time));
+        
+                while($rpr = $qpr->fetchArray())
+                {
+                   if ($deeps > 0)
+                    {
+                        $deeps--;
+                        continue;
+                    }
+                    $pars = QQ("SELECT * FROM PROSONPAR WHERE PID = ?",array($rpr['ID']));
+                    while($par = $pars->fetchArray())
+                    {
+                        $p_idx = $par['PIDX'];
+                        $p_val = $par['PVALUE'];
+
+                        // Check if it's date
+                        if ($partypes[$p_idx] == 3)
+                        {
+                            $startwhen = $rpr['STARTDATE'];
+                            $now = time();
+                            if ($now > $startwhen)
+                            {
+                                $a1 = From360ToActual($p_val);
+                                $a1 += ($now - $startwhen);
+                                $p_val = FromActualTo360($a1);
+                            }
+                        }
+                        if ($p_val == '') $p_val = 0;
+                        $sp = str_replace(sprintf('$values[%s]',$p_idx),$p_val,$sp);
+                    }
+                }
+                if (strstr($sp,'$values'))
+                    $sp = 0;
+                else
+                    $sp = eval2($sp,$uid,$cid,$placeid,$posid);
+            }
+
+        if ($debug)
+            {
+                if ($sp > 0)
+                    printf("%s: %s<br>",$rootc->attributes()['t'],$sp);
+            }
+
+
+        //  Forced
+        $forcedscore = QQ("SELECT * FROM PROSONFORCE WHERE (UID = ? OR UID = 0) AND (CID = ? OR CID = 0) AND (PLACEID = ? OR PLACEID = ? OR PLACEID = 0) AND (POS = ? OR POS = ? OR POS = 0) AND (PIDCLASS = ? OR PIDCLASS = 0)",array($uid,$cid,$placeid,$forwhichplace,$posid,$forwhichpos,$r1['PROSONTYPE']))->fetchArray();
+        if ($forcedscore)
+            {
+                $sp = $forcedscore['SCORE'];
+            }
+
+
+        $desc2['s'] = $sp;
+        $desc2['h'] = $haslist;
+        
+        $desc[] = $desc2;
+        $score += $sp;
+        }
+
+        
+    }
+
+
+    if ($posid)
+    {
+        $v = CalculateScore($uid,$cid,$placeid,0,$debug,$linkssave,$prosononly,$desc,0,$posid);
+        if ($v == -1)
+            return -1;
+        $score += $v;
+    }
+    else
+    if ($placeid)
+    {
+        $v =  CalculateScore($uid,$cid,0,0,$debug,$linkssave,$prosononly,$desc,$placeid,$forwhichpos);
+        if ($v == -1)
+            return -1;
+        $score += $v;
+    }
+
+    return $score;
+
+}
